000ed97bf7c587a6009a8a500c416918bbe913d3
mathkimchi <kingalvinjkim@gmail.com>
2024-09-01 20:25:51 -0400

    implement object io streams

--- singularity_common/src/utils/object_stream.rs
+++ singularity_common/src/utils/object_stream.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
+use std::io::{Read, Write};
 
-/// Bi-directional stream for sending objects over a stream.
 /// Objects need to be serde serializable and deserializable.
 /// The reciever's deserializer should be compatible with
 /// the sender's serializer.
@@ -10,17 +10,49 @@ use serde::{Deserialize, Serialize};
 /// Once the object is serialized into an array of u8,
 /// two u8s are sent, representing the array's u16 size in big
 /// endian. Then, the array is sent.
-pub struct ObjectStream {}
-impl ObjectStream {
-    pub fn new() -> Self {
-        todo!()
-    }
+///
+/// The array is currently in JSON format.
+pub trait ObjectInputStream {
+    fn read_object<T: for<'de> Deserialize<'de>>(&mut self) -> T;
+}
+impl ObjectInputStream for dyn Read {
+    fn read_object<T: for<'de> Deserialize<'de>>(&mut self) -> T {
+        let raw_message_length = {
+            let mut raw_message_length_buffer = [0; 2];
 
-    pub fn send<T: Serialize>(&mut self, object: T) {
-        todo!()
-    }
+            self.read_exact(&mut raw_message_length_buffer)
+                .expect("failed to read message length from subapp process");
 
-    pub fn get<'a, T: Deserialize<'a>>(&mut self) -> Option<T> {
-        todo!()
+            usize::from(u16::from_be_bytes(raw_message_length_buffer))
+        };
+
+        let mut raw_message_buffer = vec![0; raw_message_length];
+        self.read_exact(&mut raw_message_buffer)
+            .expect("failed to read message from subapp process");
+
+        serde_json::from_slice(&raw_message_buffer).expect("failed to deserialize object")
+    }
+}
+
+pub trait ObjectOutputStream {
+    fn write_object<T: Serialize>(&mut self, object: &T);
+}
+impl ObjectOutputStream for dyn Write {
+    fn write_object<T: Serialize>(&mut self, object: &T) {
+        let raw_object = serde_json::to_vec(object).expect("failed to serialize object");
+
+        // send raw object length
+        raw_object.len();
+
+        // send raw object
+        let raw_object_len = (raw_object.len() as u16).to_be_bytes();
+
+        self.write_all(&raw_object_len)
+            .expect("failed to send message length");
+
+        self.write_all(&raw_object).expect("failed to send message");
+
+        // Need to flush for buffered writers
+        self.flush().unwrap();
     }
 }
