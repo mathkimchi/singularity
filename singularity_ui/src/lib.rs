ebd74f776674307d3b705096e64aabe4aa750fff
mathkimchi <kingalvinjkim@gmail.com>
2024-09-24 20:31:51 -0400

    refactor to allow features

--- singularity_ui/src/lib.rs
+++ singularity_ui/src/lib.rs
@@ -1,5 +1,11 @@
-use iced::{executor, Command};
 use std::sync::{Arc, Mutex};
+
+#[cfg(feature = "egui_backend")]
+pub mod egui_backend;
+#[cfg(feature = "iced_backend")]
+pub mod iced_backend;
+#[cfg(not(any(feature = "egui_backend", feature = "iced_backend")))]
+compile_error!("need to choose a gui backend");
 
 pub type DisplayArea = (usize, usize);
 // pub type DisplayBuffer = Vec<u8>;
@@ -9,26 +15,6 @@ pub type UIEvent = ();
 pub enum UIElement {
     Div(Vec<UIElement>),
     Letter(char),
-}
-impl From<&UIElement> for iced::Element<'_, ()> {
-    fn from(value: &UIElement) -> Self {
-        match value {
-            UIElement::Div(vec) => {
-                iced::widget::column(vec.iter().map(|child| child.into())).into()
-            }
-            UIElement::Letter(c) => iced::widget::text(c).into(),
-        }
-    }
-}
-impl UIElement {
-    pub fn get_as_iced_element(&self) -> iced::Element<'static, ()> {
-        match self {
-            UIElement::Div(vec) => {
-                iced::widget::column(vec.iter().map(|child| child.into())).into()
-            }
-            UIElement::Letter(c) => iced::widget::button(iced::widget::text(c)).into(),
-        }
-    }
 }
 
 pub struct UIDisplay {
@@ -45,69 +31,5 @@ impl UIDisplay {
         todo!()
     }
 
-    pub fn run_display(root_element: Arc<Mutex<UIElement>>) {
-        use iced::Application;
-        // winit::event_loop::EventLoop::builder()
-        //     .with_any_thread(true)
-        //     .build()
-        //     .unwrap()
-        //     .run();
-
-        std::thread::spawn(|| {
-            Self::run(iced::Settings {
-                flags: root_element,
-
-                id: None,
-                window: iced::window::Settings::default(),
-                fonts: Vec::new(),
-                default_font: iced::Font::default(),
-                default_text_size: iced::Pixels(16.0),
-                antialiasing: false,
-            })
-            .unwrap();
-        });
-    }
+    pub fn run_display(root_element: Arc<Mutex<UIElement>>) {}
 }
-
-#[cfg(feature = "iced_backend")]
-impl iced::Application for UIDisplay {
-    type Message = ();
-    type Executor = executor::Default;
-    type Theme = iced::Theme;
-    type Flags = Arc<Mutex<UIElement>>;
-
-    fn new(flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
-        (
-            UIDisplay {
-                root_element: flags,
-            },
-            Command::none(),
-        )
-    }
-
-    fn title(&self) -> String {
-        String::from("Window")
-    }
-
-    fn update(&mut self, _message: Self::Message) -> iced::Command<()> {
-        Command::none()
-    }
-
-    fn view(&self) -> iced::Element<'_, Self::Message> {
-        dbg!(&self.root_element.lock());
-        self.root_element.lock().unwrap().get_as_iced_element()
-    }
-
-    fn theme(&self) -> Self::Theme {
-        iced::Theme::Dark
-    }
-
-    fn subscription(&self) -> iced::Subscription<Self::Message> {
-        iced::Subscription::batch(vec![iced::keyboard::on_key_press(|_, _| {
-            dbg!("Hi");
-            Some(())
-        })])
-    }
-}
-#[cfg(not(feature = "iced_backend"))]
-compile_error!("");
