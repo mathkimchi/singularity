104de7e49da3e369d15793198a6a6a5448621e63
mathkimchi <kingalvinjkim@gmail.com>
2026-07-11 18:51:04 -0400

    fix auto clippy fix lint names

--- scripts/auto_clippy_fix.sh
+++ scripts/auto_clippy_fix.sh
@@ -1,1 +1,1 @@
-cargo clippy --fix -- -A clippy::all -A clippy::pedantic -A clippy::nursery -A clippy::cargo -W clippy::use_self -W clippy::must_use_candidate -W missing_const_for_fn -W explicit_iter_loop
+cargo clippy --fix -- -A clippy::all -A clippy::pedantic -A clippy::nursery -A clippy::cargo -W clippy::use_self -W clippy::must_use_candidate -W clippy::missing_const_for_fn -W clippy::explicit_iter_loop
